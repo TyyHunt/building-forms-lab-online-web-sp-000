9f43dfd97d3f396235bd501f72887e71e1c9d058
Tyler Hunt <tyhunt04@gmail.com>
2019-08-09 19:38:27 +0000

    Automatically backed up by Learn

--- src/components/BandInput.js
+++ src/components/BandInput.js
@@ -1,7 +1,14 @@
 // Add BandInput component
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 class BandInput extends Component {
+  state = {
+    name: ''
+  }
+
+  handleChange = event => {
+    this.setState({name: event.target.value})
+  }
   render() {
     return(
       <div>
